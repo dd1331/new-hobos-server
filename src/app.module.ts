4b9942fadd79012adf832798d74045cb80c01241
Chalie Choi <ddd1331@gmail.com>
2022-09-05 01:06:07 +0900

    add some resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule, PostModule, CommentModule, LikeModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
