631eafda10a87da1774b06324fcc13d7fa67e2f6
Chalie Choi <ddd1331@gmail.com>
2022-11-16 21:08:18 +0900

    category view

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -9,11 +9,19 @@ import {
 import { Common } from '../../common/common.entity';
 import { PostCategory } from '../../post/entities/post-category.entity';
 
+enum View {
+  NORMAl = 'NORMAL',
+  SIMPLE = 'SIMPLE',
+  FANCY = 'FANCY',
+}
 @Entity()
 @Tree('materialized-path')
 export class Category extends Common {
   @Column()
   title: string;
+
+  @Column({ type: 'enum', enum: View, default: View.FANCY })
+  view: View;
 
   @ManyToOne(() => PostCategory)
   postCategory: PostCategory;
